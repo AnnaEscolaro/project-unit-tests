fa8385536b13bb59f26bac5de0ce2c5539f64b85
Anna Escolaro <annaescolaro@hotmail.com>
2023-05-08 14:49:15 -0300

    req 10

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,6 +1,29 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
 
-const createMenu = () => {};
+const createMenu = (menu) => ({
+  fetchMenu: () => menu,
+  consumption: [],
+  order(el) {
+    if (el in menu.food || el in menu.drinks) {
+      return this.consumption.push(el);
+    }
+    return 'Item indisponível';
+  },
+  pay() {
+    let dueAmount = 0;
+    this.consumption.forEach((item) => {
+      if (item in menu.food) {
+        dueAmount += menu.food[item];
+      }
+      if (item in menu.drinks) {
+        dueAmount += menu.drinks[item];
+      }
+    });
+    return (dueAmount * 1.1);
+  },
+});
+
+console.log(createMenu({ food: { pizza: 10.00 }, drinks: { coca: 3.50 } }));
 
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -1,8 +1,52 @@
 const createMenu = require('../src/restaurant');
- 
+
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
-  it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // Escreva todos os testes aqui.
+
+  it('Verifica se o objeto tem a propriedade fetchMenu e é uma function', () => {
+    const menuCreated = createMenu();
+    expect(menuCreated).toHaveProperty('fetchMenu');
+    expect(typeof (menuCreated.fetchMenu)).toBe('function');
   });
+
+  it('menu passado pra função createMenu() é idêntico ao menu recuperado pela função createMenu({ food: {}, drinks: {} }).fetchMenu()', () => {
+    expect({ food: {}, drinks: {} }).toEqual(createMenu({ food: {}, drinks: {} }).fetchMenu());
+  });
+
+  it('propriedade consumption retorna um array vazio', () => {
+    expect(createMenu({ food: {}, drinks: {} }).consumption).toEqual([]);
+  });
+
+  it('valida na order item indisponível no menu', () => {
+    const menuCreated = createMenu({ food: {}, drinks: {} });
+    const consumptionObj = menuCreated.consumption;
+    expect(menuCreated.order('coca')).toBe('Item indisponível');
+    expect(menuCreated.consumption).toEqual(consumptionObj);
+  });
+
+  it('caso item passado para order() esteja disponível no menu, adicionar a consumption', () => {
+    const menuCreated = createMenu({ food: {}, drinks: { coca: 3.50 } });
+    expect(menuCreated.order('coca')).not.toBe('Item indisponível');
+    expect(menuCreated.consumption).toContain('coca');
+  });
+
+  it('função order aceita que pedidos repetidos sejam acrescidos a consumption', () => {
+    const menuCreated = createMenu({ food: {}, drinks: { coca: 3.50 } });
+    menuCreated.order('coca');
+    menuCreated.order('coca');
+    expect(menuCreated.consumption).toEqual(['coca', 'coca']);
+  });
+
+  it('pay() deve retornar a soma dos preços de tudo que foi pedido, conforme registrado em consumption', () => {
+    const menuCreated = createMenu({ food: {}, drinks: { coca: 3.50 } });
+    menuCreated.order('coca');
+    menuCreated.order('coca');
+    expect(menuCreated.consumption).toEqual(['coca', 'coca']);
+  });
+
+  it('pay() deve retornar a soma dos preços de tudo que foi pedido, conforme registrado em consumption', () => {
+    const menuCreated = createMenu({ food: { pizza: 10.00 }, drinks: { coca: 3.50 } });
+    menuCreated.order('coca');
+    menuCreated.order('pizza');
+    expect(menuCreated.pay()).toBe(13.50 * 1.1);
+  })
 });
